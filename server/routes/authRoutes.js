8d3785cdee30c4058799b769248ea659cbcf25e3
priyansh9817 <priyanshu9817.s@gmail.com>
2025-03-03 23:51:00 +0530

    MERN

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {registerController,loginController,testController,} from "../controllers/authcontroller.js";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
+
+export default router;
